e08d52926dbf459c77c676d8de39aaa85ba22493
Daniel Corredor <danielcasw@gmail.com>
2024-09-26 15:50:47 -0500

    fix routes wildcard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,8 @@ import { TaskFormComponent } from './components/task-form/task-form.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
 
 export const routes: Routes = [
-  {path: '', component:TaskFormComponent},
-  {path: '*',component:TaskFormComponent},
-  {
-    path:'tasks-list', component:TaskListComponent
-  }
+  { path: '', redirectTo: 'tasks-form', pathMatch: 'full' }, 
+  { path: 'tasks-form', component: TaskFormComponent }, 
+  { path: 'tasks-list', component: TaskListComponent }, 
+  { path: '**', redirectTo: 'tasks-form' } 
 ];
