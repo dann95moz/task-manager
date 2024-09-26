46ce60a1e4626adb739619d214b70f0b7e70b1d0
Daniel Corredor <danielcasw@gmail.com>
2024-09-26 14:24:47 -0500

    config routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component:TaskFormComponent},
+  {path: '*',component:TaskFormComponent},
+  {
+    path:'tasks-list', component:TaskListComponent
+  }
+];
